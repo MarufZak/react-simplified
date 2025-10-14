c5f310ebc2e40aeb2818adde5f4b794974fb2b32
Ma'ruf <marufzak04@gmail.com>
2025-10-14 10:03:53 +0500

    partially migrate to webpack for react package

--- apps/dashboard/README.md
+++ apps/dashboard/README.md
@@ -14,8 +14,4 @@ Now open your browser and navigate to `http://localhost:3000`.
 
 ## Demo
 
-
-
 https://github.com/user-attachments/assets/9be8e234-d4fe-434b-848a-3f52bc847c9e
-
-
---
+++ packages/react/webpack.config.js
@@ -0,0 +1,48 @@
+import path from "node:path";
+import CopyPlugin from "copy-webpack-plugin";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  mode: isProduction ? "production" : "development",
+  entry: {
+    core: "./src/core/index.ts",
+    dom: "./src/dom/index.ts",
+  },
+  output: {
+    filename: "[name]/index.js",
+    path: path.resolve("dist"),
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./src/types/index.d.ts", to: "./dist/types" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              targets: "defaults",
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+};
+
+export default config;
